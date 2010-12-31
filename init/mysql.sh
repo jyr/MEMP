7e2d35b076c6d39646dddbda6ff3119c34bd733d
Jair Gaxiola <jyr.gaxiola@gmail.com>
2010-12-31 13:12:47 -0600

    path for mysql binaries

--- init/mysql.sh
+++ init/mysql.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-/Applications/MEMP/Library/bin/mysqld_safe --port=3306 --socket=/Applications/MEMP/tmp/mysql/mysql.sock --lower_case_table_names=0 --datadir=/Applications/MEMP/Library/data --pid-file=/Applications/MEMP/tmp/mysql/mysql.pid --log-error=/Applications/MEMP/logs/mysql_error_log &
+/Applications/MEMP/Library/mysql/bin/mysqld_safe --port=3306 --socket=/Applications/MEMP/tmp/mysql/mysql.sock --lower_case_table_names=0 --datadir=/Applications/MEMP/Library/mysql/data --pid-file=/Applications/MEMP/tmp/mysql/mysql.pid --log-error=/Applications/MEMP/logs/mysql_error_log &
