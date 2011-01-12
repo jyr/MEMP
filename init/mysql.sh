5bf656107970fe5c741bdb18598e628055ae89d3
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-01-11 23:32:30 -0600

    pat tmpdir

--- init/mysql.sh
+++ init/mysql.sh
@@ -9,7 +9,7 @@ __show_usage( ) {
 
 case "${1}" in
     start)
-        /Applications/MEMP/Library/mysql/bin/mysqld_safe --port=3306 --socket=/Applications/MEMP/tmp/mysql/mysql.sock --lower_case_table_names=0 --datadir=/Applications/MEMP/Library/mysql/data --pid-file=/Applications/MEMP/tmp/mysql/mysql.pid --log-error=/Applications/MEMP/logs/mysql_error_log &
+        /Applications/MEMP/Library/mysql/bin/mysqld_safe -u root --port=3306 --socket=/Applications/MEMP/tmp/mysql/mysql.sock --lower_case_table_names=0 --datadir=/Applications/MEMP/Library/mysql/data --pid-file=/Applications/MEMP/tmp/mysql/mysql.pid --log-error=/Applications/MEMP/logs/mysql_error_log --tmpdir=/Applications/MEMP/tmp/mysql &
         ;;
     stop)
         /Applications/MEMP/Library/mysql/bin/mysqladmin -u root --socket=/Applications/MEMP/tmp/mysql/mysql.sock shutdown
